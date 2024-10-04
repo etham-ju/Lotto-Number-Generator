c2d84a4fafcb66c46c26d027a0821dbc4290fbec
etham_ <31636309+etham-ju@users.noreply.github.com>
2024-10-04 09:54:08 +0900

    Update page.tsx
    fix build error done by chatGPT

--- app/page.tsx
+++ app/page.tsx
@@ -67,9 +67,9 @@ export default function Home() {
     return UUID_REGEX.test(uuid);
   };
 
-  const debounce = (func: Function, delay: number) => {
+  const debounce = <T extends (...args: any[]) => void>(func: T, delay: number) => {
     let timer: ReturnType<typeof setTimeout>;
-    const debounced = (...args: any[]) => {
+    const debounced = (...args: Parameters<T>) => {
       clearTimeout(timer);
       timer = setTimeout(() => func(...args), delay);
     };
